bcb41805de981f625bbc9abb0e36c5c845593261
Sujal kr Sil <sujal.sil30@gmail.com>
2024-08-23 11:52:10 +0530

    Update temp_conversion.py

--- coding_challenge/temp_conversion.py
+++ coding_challenge/temp_conversion.py
@@ -1,2 +1,4 @@
-celsius=int(input("enter temperature"))
+celsius=int(input("enter temperature in celcius"))
 print("temperature in fahrenheit: ",(9/5*celsius)+32)
+Fahrenheit=int(input("enter temperature in fahrenheit: ")
+print("temperature in celcius: ", (Fahrenheit-32)*5/9)
